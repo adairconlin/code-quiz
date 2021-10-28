67fd3c6858162a6c53493038916da7ff304c68f3
adairconlin <adairconlin@gmail.com>
2021-10-28 14:26:57 -0400

    following question now loads when answer is clicked

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
-let currentSlide = 0;
-let currentQuestion = {
+let mainBody = document.querySelector("main");
+let currentSlide = 1;
+let questionObj = {
     question1: {
         title: "Question 1",
         answers: [
@@ -30,16 +31,25 @@ let currentQuestion = {
             "1. Answer 1", "2. Answer 2", "3. Answer 3", "4. Answer 4", "5. Answer 5"
         ]
     },
-}
+};
 
 // Remove title elements from the main section.
-let clearMain = function(mainBody) {
+let clearMain = function() {
     while(mainBody.firstChild) {
         mainBody.removeChild(mainBody.firstChild);
     } 
 };
 
-let addEndGameScreen = function(mainBody) {
+// Show end game screen for user to see their score.
+let endGame = function() {
+    // Clear elements from main.
+    clearMain();
+    // Call function to load elements on main.
+    addEndGameScreen();
+};
+
+// Dynamically load end game screen.
+let addEndGameScreen = function() {
     let endSection = document.createElement("section");
     endSection.className = "endTitle";
     mainBody.appendChild(endSection);
@@ -71,16 +81,34 @@ let addEndGameScreen = function(mainBody) {
         }
         nameForm.appendChild(formInput);
     }
-}
+};
 
-// Show end game screen for user to see their score.
-let endGame = function(mainBody) {
-    clearMain(mainBody);
-    addEndGameScreen(mainBody);
-}
+let loadAnswers = function() {
+
+};
+
+// Grabs current question from questionObj based on the currentSlide value.
+let loadQuestion = function() {
+    let test = "question" + currentSlide;
+    let currentQuestion = questionObj[test].title;
+    console.log(Object.keys(questionObj).length);
+    currentSlide++;
+
+    return currentQuestion;
+};
+
+// Load the new question and corresponding answers.
+let loadQA = function() {
+    if(currentSlide <= Object.keys(questionObj).length) {
+        let question = document.querySelector(".question");
+        question.textContent = loadQuestion();
+    } else {
+        endGame();
+    }
+};
 
 // Start timer when quiz begins.
-let startTimer = function(mainBody) {
+let startTimer = function() {
     let timer = document.querySelector(".timer");
     let timeCount = 3;
     let timerFunction = setInterval(function() {
@@ -90,10 +118,10 @@ let startTimer = function(mainBody) {
         } else {
             clearInterval(timerFunction);
             timer.textContent = "Timer: " + timeCount;
-            endGame(mainBody);
+            endGame();
         }
     }, 1000);
-}
+};
 
 // Create third section of quiz that states if the answer was correct or not.
 let addFeedbackSection = function() {
@@ -103,7 +131,7 @@ let addFeedbackSection = function() {
     let feedbackHeader = document.createElement("h2");
     feedbackHeader.textContent = "Correct!";
     newSection.appendChild(feedbackHeader);
-}
+};
 
 // Create second section of quiz that contains answer choices.
 let addAnswerSection = function() {
@@ -117,7 +145,8 @@ let addAnswerSection = function() {
     for(let i = 1; i < 6; i++) {
         let btn = document.createElement("button");
         btn.id = "answer" + i;
-        btn.textContent = i + ". Answer " + i;
+        btn.className = "answers";
+        btn.textContent = questionObj.question1.answers[i - 1];
         newDiv.appendChild(btn);
     }
 };
@@ -133,13 +162,12 @@ let addQuestionSection = function() {
     // Create question header.
     let questionHeader = document.createElement("h1");
     questionHeader.className = "question";
-    questionHeader.textContent = "Lorem ipsum dolor sit amet consectetur, adipsicing elit lorem?";
+    questionHeader.textContent = loadQuestion();
     newDiv.appendChild(questionHeader);
 };
 
 let startQuiz = function() {
-    let mainBody = document.querySelector("main");
-    clearMain(mainBody);
+    clearMain();
     // Add quiz content to the main section.
     for(let i = 0; i < 3; i++) {
         if(i === 0) {
@@ -154,7 +182,15 @@ let startQuiz = function() {
         }
         mainBody.appendChild(newSection);
     }
-    startTimer(mainBody);
-}
+
+    // Add event listener to load new question on each user click.
+    let answerButtons = document.querySelectorAll(".answers");
+    answerButtons.forEach(btn => {
+        btn.addEventListener("click", loadQA);
+    });
+    
+    // Start quiz timer.
+    //startTimer(mainBody);
+};
 
 document.querySelector(".startQuiz").addEventListener("click", startQuiz);
