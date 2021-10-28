6d69df7ea4695e0f2bf48cca6f9e859f0cf24e2a
adairconlin <adairconlin@gmail.com>
2021-10-28 11:01:45 -0400

    add timer function and organized functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-let timeCount = 0;
 let currentSlide = 0;
 let currentQuestion = {
     question1: {
@@ -33,6 +32,63 @@ let currentQuestion = {
     },
 }
 
+let startTimer = function() {
+    let timer = document.querySelector(".timer");
+    let timeCount = 9;
+    let timerFunction = setInterval(function() {
+        if(timeCount > 0) {
+            timer.textContent = "Timer: " + timeCount;
+            timeCount--;
+        } else {
+            clearInterval(timerFunction);
+            timer.textContent = "Timer: " + timeCount;
+        }
+    }, 1000);
+}
+
+// Create third section of quiz that states if the answer was correct or not.
+let addFeedbackSection = function() {
+    newSection = document.createElement("section");
+    newSection.className = "feedbackSection";
+    // Create element for answer feedback.
+    let feedbackHeader = document.createElement("h2");
+    feedbackHeader.textContent = "Correct!";
+    newSection.appendChild(feedbackHeader);
+}
+
+// Create second section of quiz that contains answer choices.
+let addAnswerSection = function() {
+    newSection = document.createElement("section");
+    newSection.className = "answerSection";
+    // Create div that holds the answer buttons.
+    newDiv = document.createElement("div");
+    newDiv.className = "answerDiv";
+    newSection.appendChild(newDiv);
+
+    // Create each button dynamically with a for loop.
+    for(let i = 1; i < 6; i++) {
+        let btn = document.createElement("button");
+        btn.id = "answer" + i;
+        btn.textContent = i + ". Answer " + i;
+        newDiv.appendChild(btn);
+    }
+};
+
+// Create first section of quiz that contains the question.
+let addQuestionSection = function() {
+    newSection = document.createElement("section");
+    newSection.className = "questionSection";
+    // Create div that holds the question header.
+    let newDiv = document.createElement("div");
+    newDiv.className = "questionDiv";
+    newSection.appendChild(newDiv);
+    // Create question header.
+    let questionHeader = document.createElement("h1");
+    questionHeader.className = "question";
+    questionHeader.textContent = "Lorem ipsum dolor sit amet consectetur, adipsicing elit lorem?";
+    newDiv.appendChild(questionHeader);
+};
+
 let startQuiz = function() {
     let mainBody = document.querySelector("main");
     // Remove title elements from the main section.
@@ -40,65 +96,22 @@ let startQuiz = function() {
         mainBody.removeChild(mainBody.firstChild);
     }
 
-    // Define variables once to make them available throughout the function.
-    let newSection;
-    let newDiv;
     // Add quiz content to the main section.
     for(let i = 0; i < 3; i++) {
         if(i === 0) {
-            // Create first section of quiz that contains the question.
-            newSection = document.createElement("section");
-            newSection.className = "questionSection";
-            // Create div that holds the question header.
-            newDiv = document.createElement("div");
-            newDiv.className = "questionDiv";
-            newSection.appendChild(newDiv);
-            // Create question header.
-            let questionHeader = document.createElement("h1");
-            questionHeader.className = "question";
-            questionHeader.textContent = "Lorem ipsum dolor sit amet consectetur, adipsicing elit lorem?";
-            newDiv.appendChild(questionHeader);
-
+            let newSection;
+            addQuestionSection();
         } else if(i === 1) {
-            // Create second section of quiz that contains answer choices.
-            newSection = document.createElement("section");
-            newSection.className = "answerSection";
-            // Create div that holds the answer buttons.
-            newDiv = document.createElement("div");
-            newDiv.className = "answerDiv";
-            newSection.appendChild(newDiv);
-            // Create each individual button.
-            let btn1 = document.createElement("button");
-            let btn2 = document.createElement("button");
-            let btn3 = document.createElement("button");
-            let btn4 = document.createElement("button");
-            // Assign each button a unique id.
-            btn1.id = "answer1";
-            btn2.id = "answer2";
-            btn3.id = "answer3";
-            btn4.id = "answer4";
-            // Placeholder text content.
-            btn1.textContent = "1. Answer 1";
-            btn2.textContent = "2. Answer 2";
-            btn3.textContent = "3. Answer 3";
-            btn4.textContent = "4. Answer 4";
-
-            newDiv.appendChild(btn1);
-            newDiv.appendChild(btn2);
-            newDiv.appendChild(btn3);
-            newDiv.appendChild(btn4);
-
+            let newSection;
+            addAnswerSection();
         } else if(i === 2) {
-            // Create third section of quiz that states if the answer was correct or not.
-            newSection = document.createElement("section");
-            newSection.className = "feedbackSection";
-            // Create element for answer feedback.
-            let feedbackHeader = document.createElement("h2");
-            feedbackHeader.textContent = "Correct!";
-            newSection.appendChild(feedbackHeader);
+            let newSection;
+            addFeedbackSection();
         }
         mainBody.appendChild(newSection);
     }
+
+    startTimer();
 }
 
 document.querySelector(".startQuiz").addEventListener("click", startQuiz);
